8fe0676e17da96c5b282338600fcfee0f22270bf
avship <alexandershipilo@yandex.ru>
2022-06-21 15:37:53 +0300

    Выполненный хард 9ого урока

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-"use strict";
-
-const week = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
-];
-
-let currentDate = new Date().getDay();
-if (currentDate === 0) {
-  currentDate = 6;
-} else {
-  currentDate -= 1;
-}
-for (let i = 0; i < week.length; i++) {
-  let tmpBegin = "<p>";
-  let tmpEnd = "</p>";
-  if (i >= 5) {
-    tmpBegin += "<i>";
-    tmpEnd = "</i>" + tmpEnd;
+let now = new Date();
+let options = {
+  //era: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  weekday: "long",
+  timezone: "UTC",
+  // hour: "numeric",
+  // minute: "numeric",
+  // second: "numeric",
+};
+let formattedDate = now
+  .toLocaleString("ru", options)
+  .replace("г.", "года")
+  .replace("  ", " ");
+function hourFormatter(now) {
+  if (now.getHours() % 10 == 1) {
+    return "час";
   }
-  if (i === currentDate) {
-    tmpBegin += "<b>";
-    tmpEnd = "</b>" + tmpEnd;
+  if (now.getHours() < 5 && Math.floor(now.getHours() / 10)) return "часа";
+  return "часов";
+}
+function minuteFormatter(now) {
+  if (now.getMinutes() % 10 == 1) {
+    return "минута";
   }
-  week[i] = tmpBegin + week[i] + tmpEnd;
+  if (now.getMinutes() % 10 < 5 && now.getMinutes() % 10 > 0) {
+    return "минуты";
+  }
+  return "минут";
+}
+function secondsFormatter(now) {
+  if (now.getSeconds() % 10 == 1) {
+    return "секунда";
+  }
+  if (now.getSeconds() % 10 < 5 && now.getSeconds() % 10 > 0) {
+    return "секунды";
+  }
+  return "секунд";
+}
+const hour = `${now.getHours()} ${hourFormatter(now)}`;
+const minutes = `${now.getMinutes()} ${minuteFormatter(now)}`;
+const seconds = `${now.getSeconds()} ${secondsFormatter(now)}`;
+//a) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
+console.log(`Сегодня ${formattedDate} ${hour} ${minutes} ${seconds}`);
+
+//б) '04.02.2020 - 21:05:33'
+function addZeroToDateValues() {
+  const date = new Date();
+  const day = date.getDay() < 10 ? `0${date.getDay()}` : `${date.getDay()}`;
+  const month =
+    date.getMonth() < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+  const hour =
+    date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
+  const minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+  const seconds =
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : `${date.getSeconds()}`;
+  return `${day}.${month}.${date.getFullYear()} - ${hour}:${minutes}:${seconds}`;
+}
+console.log(addZeroToDateValues());
+function clock() {
+  const dateDOM = document.querySelector("#days");
+  dateDOM.textContent = addZeroToDateValues();
 }
 
-document.querySelector("#days").innerHTML = week.join("\n");
-//console.log(currentDate, week);
+setInterval(clock, 1000);
